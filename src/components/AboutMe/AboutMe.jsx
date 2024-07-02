26a71ae9f84a604b3713c8d08230bad4cb0e19c5
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-07-03 00:58:18 +0600

    My Portfolio Website Update

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -13,16 +13,18 @@ const AboutMe = () => {
 
 
     return (
-        <section id='about' className=' bg-[#110f17] min-h-screen py-20'>
-            <div data-aos="fade-up" data-aos-duration="1000" className='text-center'>
+        <section id='about' className=' bg-[#110f17] min-h-screen py-10'>
+            {/* <div data-aos="fade-up" data-aos-duration="1000" className='text-center'>
                 <h1 className='text-center text-2xl text-white'>About Me</h1>
                 <div className='border-2 border-[#07dde1] w-20 m-auto'></div>
-            </div>
-            <div className='lg:mx-32 lg:my-20 my-5 flex flex-col lg:flex-row justify-center items-center space-x-10 space-y-3'>
+            </div> */}
+            <div className='lg:mx-32 lg:my-20  flex flex-col lg:flex-row justify-center items-center space-x-10 space-y-3'>
                 <div data-aos="fade-right" data-aos-delay="1000" className='lg:w-[40%] mx-5 text-white'>
-                    <Image width={500} height={300} unoptimized className='w-full rounded-lg' src="/p.gif" alt="" />
+                    <Image width={500} height={300}  className='w-full rounded-lg' src="/man2.png" alt="" />
                 </div>
                 <div data-aos="fade-right" data-aos-delay="1000" className='lg:w-[50%]  text-white space-y-5'>
+                    <h1 className=' text-4xl text-white'>About Me</h1>
+                    <h2 className='text-2xl'>Front End Developer</h2>
                     <p data-aos="fade-right" data-aos-delay="1000" className=''>I am Kalidash Odekare. I am a web developer. And I am very passionate and dedicated to my work. As per Bayer's instructions in my 1 year experience. We develop user friendly websites.</p>
                     <button data-aos="fade-right" data-aos-delay="1000" className='btn border text-white border-[#07dde1] bg-opacity-0'>Ream More</button>
                 </div>
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,8 +8,8 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './Banner.css'
 import { Typewriter } from 'react-simple-typewriter'
-import { RiJavascriptFill, RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
-import { SiExpress } from "react-icons/si";
+import Link from 'next/link';
+
 
 
 const Banner = () => {
@@ -23,7 +23,7 @@ const Banner = () => {
     }, [])
 
     return (
-        <div id='home' className=' bg-[#0d0d0d] flex flex-col-reverse lg:flex-row justify-center items-center space-y-10 min-h-screen'>
+        <div id='home' className=' bg-[#0d0d0d] flex flex-col-reverse lg:flex-row justify-center items-center space-y-10 min-h-screen lg:px-32'>
             {
                 isClient && !isMobile && <Cursor />
             }
@@ -41,42 +41,26 @@ const Banner = () => {
 
                     />
                 </h1>
-                <p data-aos="fade-right" data-aos-delay="2000" className='lg:w-[60%] text-center lg:text-left'>I am a front end developer. I develop customer and user friendly websites on demand</p>
+                <p data-aos="fade-right" data-aos-delay="2000" className='lg:w-[60%] text-center lg:text-left'>I am a frontend developer, proficient in HTML, CSS, JavaScript, React, Tailwind CSS, Next.js, Node.js, and MongoDB. My goal is to create high quality web applications and improve user experience.</p>
                 <div className='flex lg:justify-start justify-center items-center space-x-5 text-2xl'>
-                    <FaFacebook data-aos="fade-up" data-aos-delay="2200" />
+                    <a target='_blank' href="https://www.facebook.com/Shakti.Adhikari.68">
+                        <FaFacebook data-aos="fade-up" data-aos-delay="2200" />
+                    </a>
                     <FaInstagram data-aos="fade-up" data-aos-delay="2300" />
                     <FaTwitter data-aos="fade-up" data-aos-delay="2400" />
                     <FaLinkedin data-aos="fade-up" data-aos-delay="2500" />
                 </div>
+                <div>
+                    <Link data-aos="fade-up" data-aos-delay="2600" download href="/dawnload.pdf">
+                        <button className='btn bg-opacity-0 border text-white  hover:bg-[#07dde1] hover:border-[#07dde1] ease-in duration-300 hover:text-black border-[#07dde1]'>Dawnload Resume</button>
+                    </Link>
+                </div>
             </div>
-            <div className='relative p-9'>
-                <div className='w-60 rounded-full border'>
-                    <img className='rounded-full ' src="/man.png" alt="" />
+            <div className='relative '>
+                <div data-aos="fade-up" data-aos-duration="1000" className='w-80'>
+                    <img className='flashMove' src="/man.png" alt="" />
                 </div>
-                <div className='absolute top-11'>
-                    <FaHtml5 className='text-2xl text-[#d94123] flashMove' />
-                </div>
-                <div className='absolute top-0 left-[140px]'>
-                    <FaCss3 className='text-2xl text-[#1d95df] flashMove1' />
-                </div>
-                <div className='absolute top-[58px] left-[260px]'>
-                    <RiJavascriptFill className='text-2xl text-[#efd81d] flashMove2' />
-                </div>
-                <div className='absolute top-[150px] left-[278px]'>
-                    <RiTailwindCssFill className='text-2xl text-[#06b0cd] flashMove3' />
-                </div>
-                <div className='absolute top-[280px] left-[250px]'>
-                    <FaReact className='text-2xl text-[#06b0cd] flashMove4' />
-                </div>
-                <div className='absolute top-[320px] left-[140px]'>
-                    <RiNextjsFill className='text-2xl bg-white rounded-full text-[#000000] flashMove4' />
-                </div>
-                <div className='absolute top-[270px] left-[25px]'>
-                    <FaNode className='text-2xl bg-white rounded-full text-[#3a8338] flashMove4' />
-                </div>
-                <div className='absolute top-[125px] left-[3px]'>
-                    <SiExpress className='text-2xl bg-white rounded-full text-[#3a8338] flashMove4' />
-                </div>
+
             </div>
 
         </div>
