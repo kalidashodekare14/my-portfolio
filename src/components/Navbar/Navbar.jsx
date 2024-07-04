d607c08818cceea89994f132b024421872a92d67
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-07-04 19:29:55 +0600

    Portfolio Website Final Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
     </>
 
     return (
-        <nav className="navbar fixed bg-[#0d0d0d] lg:px-32">
+        <nav className="navbar z-50 fixed bg-[#0d0d0d] lg:px-32">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost  text-white lg:hidden">
